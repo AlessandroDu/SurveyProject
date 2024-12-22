138e3aa2f21f1a3b47dcc655dfc196292da4338f
Alessandro Ducci <duccialessandro001@gmail.com>
2024-12-23 02:16:42 +0400

    MODELS: added __str__ method to models, created question objects and tested them plus how to send this data to frontend. Now committing working code for if I break the database in the next step.

--- surveysApp/models.py
+++ surveysApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    question = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.question
+
+
+class EmailAnswer(models.Model):
+    questionID = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+
+class OpenAnswer(models.Model):
+    questionID = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class Options(models.Model):
+    questionID = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    optionText = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.optionText
--- surveysApp/views.py
+++ surveysApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from .models import Questions
+
 
 def home(request):
     return render(request, "surveysApp/home.html")
 
 
 def index(request):
-    return render(request, "surveysApp/index.html")
+    questions = Questions.objects.get(pk=1)
+    return render(request, "surveysApp/index.html", {"questions": questions})
